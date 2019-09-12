601e098b07c40bcfb9c5e15a6f6f65268b89194c
IvanChibisov <knigaykinjan@gmail.com>
2019-09-13 02:23:05 +0300

    Changed programm code, add class with one method

--- Ruby task#1/tasc_1.rb
+++ Ruby task#1/tasc_1.rb
@@ -1,34 +1,8 @@
 class Cypher
-  def encrypt(key, file)
-    $stdin = File.open(file)
-    text = []
-    while line = gets
-      ans = ''
-      line.each_byte do |c|
-      if (c > 96) and (c < 123)
-          if c + key > 122 
-            ans << c + key - 26
-          else ans << c + key
-          end
-        end 
-      end
-      text << ans
-    end
-    text
-  end
-
-  def decrypt(key, text)
+  def crypt(key, text)
     answer_text = []
     text.each do |line|
-      ans = ''
-      line.each_byte do |c|
-      if (c > 96) and (c < 123)
-          if c - key < 96 
-            ans << c - key + 26
-          else ans << c - key
-          end
-        end 
-      end
+      ans = yield key, line
       answer_text << ans
     end
     answer_text
@@ -37,11 +11,38 @@ end
 
 
 key = 3
+text = []
 file = "input.txt"
+$stdin = File.open(file)
+while line = gets
+  text << line
+end
 process = Cypher.new
-encrypt_text = process.encrypt(key, file)
+encrypt_text = process.crypt(key, text) do |k, line; ans|
+  ans = ''
+  line.each_byte do |c|
+    if (c > 96) and (c < 123)
+      if c + k > 122 
+        ans << c + k - 26
+      else ans << c + k
+      end
+    end
+  end 
+ ans
+ end
 puts "Encrypt text:"
 encrypt_text.each { |e| puts e }
-decrypt_text = process.decrypt(key, encrypt_text)
+decrypt_text = process.crypt(key, encrypt_text) do |k, line; ans|
+  ans = ''
+  line.each_byte do |c|
+    if (c > 96) and (c < 123)
+      if c - k < 96 
+        ans << c - k + 26
+      else ans << c - k
+      end
+    end
+  end 
+ ans
+ end
 puts "Decrypt text:"
 decrypt_text.each { |e| puts e }
