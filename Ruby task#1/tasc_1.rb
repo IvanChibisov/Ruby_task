27d9b20bbe6701ce0c8859017500ceb06502c089
IvanChibisov <knigaykinjan@gmail.com>
2019-09-13 17:34:54 +0300

    Add tast#1 with one method for encrypt/decrypt

--- Ruby task#1/tasc_1.rb
+++ Ruby task#1/tasc_1.rb
@@ -2,8 +2,18 @@ class Cypher
   def crypt(key, text)
     answer_text = []
     text.each do |line|
-      ans = yield key, line
-      answer_text << ans
+      ans = ''
+      line.each_byte do |c|
+      if (c > 96) and (c < 123)
+        if c + key > 122 
+          ans << c + key - 26
+        elsif c + key < 96
+          ans << c + key + 26
+        else ans << c + key
+        end
+      end
+     end
+    answer_text << ans
     end
     answer_text
   end  
@@ -18,31 +28,9 @@ while line = gets
   text << line
 end
 process = Cypher.new
-encrypt_text = process.crypt(key, text) do |k, line; ans|
-  ans = ''
-  line.each_byte do |c|
-    if (c > 96) and (c < 123)
-      if c + k > 122 
-        ans << c + k - 26
-      else ans << c + k
-      end
-    end
-  end 
- ans
- end
+encrypt_text = process.crypt(key, text)
 puts "Encrypt text:"
 encrypt_text.each { |e| puts e }
-decrypt_text = process.crypt(key, encrypt_text) do |k, line; ans|
-  ans = ''
-  line.each_byte do |c|
-    if (c > 96) and (c < 123)
-      if c - k < 96 
-        ans << c - k + 26
-      else ans << c - k
-      end
-    end
-  end 
- ans
- end
+decrypt_text = process.crypt(-1 * key, encrypt_text)
 puts "Decrypt text:"
 decrypt_text.each { |e| puts e }
